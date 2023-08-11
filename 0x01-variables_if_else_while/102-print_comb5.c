1cef2df25ba98482f0269ecb13b8d1684160dafc
Nader Nassar <nader.reda.ebrahim@gmail.com>
2023-08-11 14:57:49 -0700

    102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,24 +12,18 @@ int main(void)
 {
 	int nums1, nums2;
 
-	for (nums1 = '0'; nums1 <= '99'; nums1++)
+	for (nums1 = 0; nums1 <= 99; nums1++)
 	{
-		for (nums2 = '0'; nums2 <= '99'; nums2++)
+		for (nums2 = 0; nums2 <= 99; nums2++)
 		{
 			if (nums1 != nums2 && nums2 > nums1)
 			{
-				if (nums1 < 10)
-				{
-					putchar('0');
-				}
-				putchar(nums1);
+				putchar(nums1 / 10 + '0');
+				putchar(nums1 % 10 + '0');
 				putchar(' ');
-				if (nums2 < 10)
-				{
-					putchar('0');
-				}
-				putchar(nums2);
-				if (nums1 != '98' || nums2 != '99')
+				putchar(nums2 / 10 + '0');
+				putchar(nums2 % 10 + '0');
+				if (nums1 != 98 || nums2 != 99)
 				{
 					putchar(',');
 					putchar(' ');
