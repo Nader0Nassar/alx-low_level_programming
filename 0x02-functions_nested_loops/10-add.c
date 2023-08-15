fd372eb2040741281dd1a312bdf5072c06a3f009
Nader Nassar <nader.reda.ebrahim@gmail.com>
2023-08-15 14:43:45 -0700

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - This function adds two numbers
+ *
+ * @num1: The first number
+ * @num2: The second number
+ *
+ * Return: On success 1.
+ */
+
+int add(int num1, int num2);
+{
+	return (num1 + num2);
+}
