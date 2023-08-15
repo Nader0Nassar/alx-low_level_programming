d1611b644316a99414c3ba657eca5d9d17b83836
Nader Nassar <nader.reda.ebrahim@gmail.com>
2023-08-15 14:32:10 -0700

    Task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table, starting with 0
+ *
+ *
+ * Return: On success 1.
+ */
+
+void times_table(void)
+{
+	int num1, num2, num3;
+
+	for (num1 = 0; num1 <= 9; num++)
+	{
+		putchar('0');
+		putchar(',');
+		for (num2 = 1; num2 <= 9; num2++)
+		{
+			num3 = num1 * num2;
+			if (num3 <= 9)
+			{
+				putchar(' ');
+				putchar(' ');
+				putchar(num3 + '0');
+				if (num2 < 9)
+					putchar(',');
+			}
+			else
+			{
+				putchar(' ');
+				putchar(num3 / 10 + '0');
+				putchar(num3 % 10 + '0');
+				if (num2 < 9)
+					putchar(',');
+			}
+		}
+		putchar('\n');
+	}
+}
