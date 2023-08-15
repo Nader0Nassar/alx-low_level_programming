4e4de809332b246c4492a18bf6b3afcf9ede65a5
Nader Nassar <nader.reda.ebrahim@gmail.com>
2023-08-15 16:17:26 -0700

    Task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,38 +12,38 @@ void print_times_table(int n)
 
 	for (num1 = 0; num1 <= n; num1++)
 	{
-		putchar('0');
-		putchar(',');
+		_putchar('0');
+		_putchar(',');
 		for (num2 = 1; num2 <= n; num2++)
 		{
 			num3 = num1 * num2;
-			putchar(' ');
+			_putchar(' ');
 			if (num3 <= 9)
 			{
-				putchar(' ');
-				putchar(' ');
-				putchar(num3 + '0');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(num3 + '0');
 				if (num2 < n)
-					putchar(',');
+					_putchar(',');
 			}
 			else if (num3 <= 99)
 			(
-				putchar(' ');
-				putchar(num3 / 10 + '0');
-				putchar(num3 % 10 + '0');
+				_putchar(' ');
+				_putchar(num3 / 10 + '0');
+				_putchar(num3 % 10 + '0');
 				if (num2 < n)
-					putchar(',');
+					_putchar(',');
 			}
 			else
 			{
-				putchar(num3 / 100 + '0');
-				putchar((num3 / 10) % 10 + '0');
-				putchar(num3 % 10 + '0');
+				_putchar(num3 / 100 + '0');
+				_putchar((num3 / 10) % 10 + '0');
+				_putchar(num3 % 10 + '0');
 				if (num2 < n)
-					putchar(',');
+					_putchar(',');
 			}
 		}
-		putchar('\n');
+		_putchar('\n');
 	}
 	}
 }
