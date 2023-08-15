2e6eb59aac2cf4872565a50cea84fcee69d6d629
Nader Nassar <nader.reda.ebrahim@gmail.com>
2023-08-15 15:10:01 -0700

    Task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,12 +11,24 @@
 
 void print_to_98(int n)
 {
-	while (n <= 98)
+	if (n > 98)
 	{
-		if (n == 98)
-			printf("%d ", n);
-		else
+		while (n > 98)
+		{
 			printf("%d, ", n);
-		n++;
+			n++;
+		}
+		printf("98");
+	}
+	else
+	{
+		while (n <= 98)
+		{
+			if (n == 98)
+				printf("%d ", n);
+			else
+				printf("%d, ", n);
+			n++;
+		}
 	}
 }
