deb2b82464e542dbe99b47e5dbc7694fa2c05252
Nader Nassar <nader.reda.ebrahim@gmail.com>
2023-08-16 04:35:48 -0700

    Task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,10 +13,10 @@ int main(void)
 
 	i = 0;
 
-	while (i < 10) /* i never increments so it will always be less than 10*/
-	{
-		putchar(i);
-	}
+	/*while (i < 10)*/
+	/*{*/
+	/*	putchar(i);*/
+	/*}*/
 
 	printf("Infinite loop avoided! \\o/\n");
 
