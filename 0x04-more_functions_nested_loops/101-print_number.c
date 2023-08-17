cb1e3488f3166654889b49c3c1d2df183b48c711
root <root@DESKTOP-46789GH.localdomain>
2023-08-17 18:21:33 +0300

    Task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -11,12 +11,14 @@
 
 void print_number(int n)
 {
+	unsigned int new_N = n;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -1 * n;
+		new_N = -1 * n;
 	}
-	if ((n / 10) > 0)
-		print_number(n / 10);
-	_putchar('0' + (n % 10));
+	if ((new_N / 10) > 0)
+		print_number(new_N / 10);
+	_putchar('0' + (new_N % 10));
 }
