a549b0de2598c4ce8bdabcc099560255f4b8f709
root <root@DESKTOP-46789GH.localdomain>
2023-08-21 22:43:51 +0300

    Task 7

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_array -  This function prints the first n items of an integer array.
+ *
+ * @a: integer array
+ * @n: The number of printed elements
+ *
+ * Return: Always 0
+ */
+
+void print_array(int *a, int n)
+{
+	int index;
+
+	for (index = 0; index < n; index++)
+	{
+		if (index != n - 1)
+		{
+			printf("%d, ", a[index]);
+		}
+		else
+		{
+			printf("%d\n", a[index]);
+		}
+	}
+}
