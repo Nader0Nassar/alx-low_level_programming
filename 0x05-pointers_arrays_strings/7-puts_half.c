8b12f84f70457cff7f746b3607162c2997f203e5
root <root@DESKTOP-46789GH.localdomain>
2023-08-21 22:15:25 +0300

    Task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * puts_half -  prints half of a string
+ *
+ * @str: The string to be printed
+ *
+ * Return: Always 0
+ */
+
+void puts_half(char *str)
+{
+	int i = 0, index = 0, length = 0;
+
+	while (str[i] != '\0')
+	{
+		length++;
+		i++;
+	}
+	if (length % 2 == 0)
+	{
+		for (index = length / 2; index < length; index++)
+		{
+			_putchar(str[index]);
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		for (index = (length - 1) / 2; index < length; index++)
+		{
+			_putchar(str[index]);
+		}
+		_putchar('\n');
+	}
+}
