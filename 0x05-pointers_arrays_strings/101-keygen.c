101f08de56c45c5f0f67f5b64dafa5c25e366f43
root <root@DESKTOP-46789GH.localdomain>
2023-08-22 03:27:18 +0300

    Task 101

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,36 +1,26 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <time.h>
-#include <string.h>
-
+#include <stdlib.h>
 
 /**
  * main - creat random password.
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
-	int i, pass_Length = 20, type_of_Char;
-	char random_Password[pass_Length + 1];
-    char lower[] = "abcdefghijklmnopqrstuvwxyz";
-    char upper[] = "ABCDEFGHIJKLMNPQRSTUVWXYZ";
-    char digit[] = "0123456789";
-    char symbol[] = "@#$^&*(){}_|<>,./?";    
-    srand(time(NULL));
-    for (i = 0; i < pass_Length; i++)
-    {
-        type_of_Char = rand() % 4;
-        if (type_of_Char == 0)
-            random_Password[i] = lower[rand() % sizeof(lower) - 1];
-        else if (type_of_Char == 1)
-            random_Password[i] = upper[rand() % sizeof(upper) - 1];
-        else if (type_of_Char == 2)
-            random_Password[i] = digit[rand() % sizeof(digit) - 1];
-        else if (type_of_Char == 0)
-            random_Password[i] = symbol[rand() % sizeof(symbol) - 1];
-    }
-    random_Password[pass_Length] = '\0';
-    printf("%s\n", random_Password);
+	char random_Char;
+	int i = 2645, j = 2772, chars_Ascii = 0;
+
+	srand(time(NULL));
+	while (chars_Ascii <= i)
+	{
+		random_Char = rand() % 128;
+		chars_Ascii = chars_Ascii + random_Char;
+		printf("%c", random_Char);
+	}
+	printf("%c", j - chars_Ascii);
+
 	return (0);
 }
