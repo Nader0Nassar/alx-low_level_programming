f114093899c92b97efc75977d5e8eae7216c7b8d
root <root@DESKTOP-46789GH.localdomain>
2023-08-24 18:39:39 +0300

    Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,7 +13,7 @@ char *cap_string(char *ls)
 
 	for (i = 0; ls[i] != '\0'; i++)
 	{
-		if (ls[i] == ' ' || ls[i] == '\t' || ls[i] == '\n' ||
+		if (i = 0 || ls[i] == ' ' || ls[i] == '\t' || ls[i] == '\n' ||
 			ls[i] == ',' || ls[i] == ';' || ls[i] == '.' || ls[i] == '!' ||
 			ls[i] == '?' || ls[i] == '"' || ls[i] == '(' || ls[i] == ')' ||
 			ls[i] == '{' || ls[i] == '}')
