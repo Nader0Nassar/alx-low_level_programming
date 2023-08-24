fba25b758fd57d566d7550fda42be17ac96e9715
root <root@DESKTOP-46789GH.localdomain>
2023-08-24 21:06:43 +0300

    Task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * leet - This function encodes a string into 1337
+ * @coded_string: The string will be coded.
+ *
+ * Return: Always 0
+ */
+
+char *leet(char *coded_string)
+{
+	int index1, index2;
+	char coded_char[] = "AEOTLaeotl";
+	char code[] = "4370143701";
+
+	for (index1 = 0; coded_string[index1] != '\0'; index1++)
+	{
+		for (index2 = 0; coded_char[index2] != '\0'; index2++)
+		{
+			if (coded_string[index1] == coded_char[index2])
+			{
+				coded_string[index1] = code[index2];
+			}
+		}
+	}
+	return (coded_string);
+}
