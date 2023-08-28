f2d113d0adf44bdef7493992268f835c611bed42
root <root@DESKTOP-46789GH.localdomain>
2023-08-28 18:47:40 +0300

    Task 7

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_chessboard -  This function prints chessboard
+ *
+ * @a: rows
+ *
+ * Return: void
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int index1, index2;
+
+	for (index1 = 0; index1 < 8; index1++)
+	{
+		for (index2 = 0; index2 < 8; index2++)
+		{
+			_putchar(a[index1][index2]);
+		}
+		_putchar('\n');
+	}
+}
