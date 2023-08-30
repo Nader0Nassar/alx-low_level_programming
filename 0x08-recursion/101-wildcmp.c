d4a911ad831211f693e8bad17bffb8eda2322ffb
root <root@DESKTOP-46789GH.localdomain>
2023-08-30 15:40:54 +0300

    Task 8

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * wildcmp - This function checks if two strings are identical or not.
+ *
+ * @s1: 1st string
+ * @s2: 2nd string
+ *
+ * Return: 1 or 0
+ */
+
+int wildcmp(char *s1, char *s2) 
+{
+	if (*s1 == '\0')
+	{
+		if (*s2 != '\0')
+		{
+			if (*s2 == '*')
+                return (wildcmp(s1, s2 + 1));
+		}
+		if (*s2 == '\0')
+            return (1);        
+	}
+	if (*s2 == '*')
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	else if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	return (0);
+}
