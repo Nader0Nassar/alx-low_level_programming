b0e4fd0e568704f9ea30619d944416544233dba5
root <root@DESKTOP-46789GH.localdomain>
2023-09-05 23:21:01 +0300

    Task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,9 +14,6 @@ char *str_concat(char *s1, char *s2)
 	int index = 0, length_s1 = strlen(s1), length_s2 = strlen(s2);
 	char *concatenated_String;
 
-	printf("\nlength s1 = %d", length_s1);
-	printf("\nlength s2 = %d\n", length_s2);
-
 
 	if (s1 == NULL)
 		s1 = "";
@@ -33,6 +30,5 @@ char *str_concat(char *s1, char *s2)
 			*(concatenated_String + index) = s2[index - length_s1];
 		index++;
 	}
-	printf("concatenated_String = %s\n", concatenated_String);
 	return (concatenated_String);
 }
