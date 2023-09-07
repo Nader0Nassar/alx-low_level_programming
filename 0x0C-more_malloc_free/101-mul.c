cd6b0fd0eba1e6a3951585d064644afb15876ab4
root <root@DESKTOP-46789GH.localdomain>
2023-09-08 00:34:56 +0300

    Task 5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -31,7 +31,10 @@ int main(int argc, char **argv)
 	length_multip = length_num1 + length_num2 + 1;
 	multip = malloc(sizeof(int) * length_multip);
 	if (multip == NULL)
-		return (NULL);
+	{
+		_puts("Error");
+		exit(98);
+	}
 	while (index < length_multip)
 	{
 		multip[index] = 0;
