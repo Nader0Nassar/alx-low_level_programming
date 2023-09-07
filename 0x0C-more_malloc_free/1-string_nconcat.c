e7ea3158a0b621eab939d69a96ccc9f01715e73e
root <root@DESKTOP-46789GH.localdomain>
2023-09-07 14:00:12 +0300

    Task 1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * string_nconcat - This function concatenates string s1 and n bytes of s2
+ * @s1: 1st string
+ * @s2: 2nd string
+ * @n: number of bytes from 2nd string
+ * Return: Pointer or Null
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int index, length_s1;
+	char *concatenated_String;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	index = 0;
+	length_s1 = strlen(s1);
+	concatenated_String = malloc(length_s1 + n + 1);
+	if (concatenated_String == NULL)
+		return (NULL);
+	while (index < length_s1 + n)
+	{
+		if (index < length_s1)
+			*(concatenated_String + index) = s1[index];
+		else if (index >= length_s1 && index < length_s1 + n)
+			*(concatenated_String + index) = s2[index - length_s1];
+		index++;
+	}
+	return (concatenated_String);
+}
