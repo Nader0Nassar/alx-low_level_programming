ed81325e959051d8928aa00361f4c484bc387b59
root <root@DESKTOP-46789GH.localdomain>
2023-09-07 23:28:01 +0300

    Task 5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,9 +2,8 @@
 #include <stdlib.h>
 
 void _puts(char *);
-unsigned long int _atoi(char *);
-void print_number(unsigned long int);
 int _isdigit(char *);
+int _strlen(char *);
 
 /**
  * main - Main Function
@@ -15,17 +14,83 @@ int _isdigit(char *);
  */
 int main(int argc, char **argv)
 {
+	char *num1, *num2;
+	int *multip;
+	int length_num1, length_num2, length_multip, index = 0;
+	int index1, index2, n1, n2, rem, multip_index = 0;
+
 	if (argc != 3 || !_isdigit(argv[1]) || !_isdigit(argv[2]))
 	{
 		_puts("Error");
 		exit(98);
 	}
-	else
+	num1 = argv[1];
+	num2 = argv[2];
+	length_num1 = _strlen(num1);
+	length_num2 = _strlen(num2);
+	length_multip = length_num1 + length_num2 + 1;
+	multip = malloc(sizeof(int) * length_multip);
+	if (multip == NULL)
+		return (1);
+	while (index < length_multip)
+		multip[index++] = 0;
+	index1 = length_num1 - 1;
+	while (index1 >= 0)
 	{
-		print_number(_atoi(argv[1]) * _atoi(argv[2]));
-		_putchar('\n');
+		n1 = num1[index1] - 48;
+		rem = 0;
+		index2 = length_num2 - 1;
+		while (index2 >= 0)
+		{
+			n2 = num2[index2] - 48;
+			rem = rem + multip[index1 + index2 + 1] + (n1 * n2);
+			multip[index1 + index2 + 1] = rem % 10;
+			rem = rem / 10;
+			index2--;
+		}
+		if (rem > 0)
+			multip[index1 + index2 + 1] = multip[index1 + index2 + 1] + rem;
+		index1--;
 	}
+	for (multip_index = 0; multip_index < length_multip - 1; multip_index++)
+	{
+		if (multip[multip_index])
+			_putchar(multip[multip_index] + 48);
+	}
+
+	_putchar('\n');
+	free(multip);
 	return (0);
+}
+/**
+ * _puts -  prints a string
+ * @str: The printed string
+ * Return: void
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
+/**
+ * _strlen -  returns the length of a string
+ * @s: string to compute the length of it
+ * Return: Always 0
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+	return (i);
 }
 /**
  * _isdigit - checks for digits 0:9
@@ -43,51 +108,4 @@ int _isdigit(char *string)
 		index++;
 	}
 	return (1);
-}
-/**
- * print_number - prints a number
- * @n:  The printed number
- * Return: void
- */
-void print_number(unsigned long int n)
-{
-	if ((n / 10) > 0)
-		print_number(n / 10);
-	_putchar('0' + (n % 10));
-}
-/**
- * _atoi - convert a string to an integer
- * @s: string
- * Return: number
- */
-unsigned long int _atoi(char *s)
-{
-	int index = 0;
-	unsigned long int value = 0;
-
-	while (s[index] != '\0')
-	{
-		if (s[index] >= 48 && s[index] <= 57)
-		{
-			value = value * 10 + s[index] - 48;
-			if (s[index + 1] < 48 || s[index + 1] > 57)
-				break;
-		}
-		index++;
-	}
-	return (value);
-}
-/**
- * _puts -  prints a string
- * @str: The printed string
- * Return: void
- */
-void _puts(char *str)
-{
-	while (*str != '\0')
-	{
-		_putchar(*str);
-		str++;
-	}
-	_putchar('\n');
 }
