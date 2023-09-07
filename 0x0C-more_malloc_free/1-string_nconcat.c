ae5d95f7f14c8b24717cca8f10a97c6b686f8beb
root <root@DESKTOP-46789GH.localdomain>
2023-09-07 14:03:35 +0300

    Task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,7 +11,7 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int index, length_s1;
+	unsigned int index, length_s1, length_s2;
 	char *concatenated_String;
 
 	if (s1 == NULL)
@@ -20,6 +20,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s2 = "";
 	index = 0;
 	length_s1 = strlen(s1);
+    length_s2 = strlen(s2);
+    if (n >= length_s2)
+        n = length_s2;
 	concatenated_String = malloc(length_s1 + n + 1);
 	if (concatenated_String == NULL)
 		return (NULL);
