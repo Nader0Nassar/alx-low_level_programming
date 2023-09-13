b8bbd1bba6585551e1b97cd59677209c0082c5d5
root <root@DESKTOP-46789GH.localdomain>
2023-09-13 12:40:56 +0300

    Tasks from 0 to 4

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,4 +1,3 @@
-#include "function_pointers.h"
 #include "3-calc.h"
 #include <stdlib.h>
 #include <stdio.h>
@@ -11,23 +10,28 @@
  */
 int main(int argc, char **argv)
 {
+	int num1, num2;
+	char *oper;
+
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-
-	if (argv[2][1] != '\0' || get_op_func(argv[2]) == NULL)
+	num1 = atoi(argv[1]);
+	num2 = argv[3];
+	oper = argv[2];
+	if (oper[1] != '\0' || get_op_func(oper) == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	if ((atoi(argv[2]) == '/' || atoi(argv[2]) == '%') && (atoi(argv[3]) == 0))
+	if ((*oper == '/' || *oper == '%') && (num2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
-	printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
+	printf("%d\n", get_op_func(oper)(num1, num2));
 
 	return (0);
 }
