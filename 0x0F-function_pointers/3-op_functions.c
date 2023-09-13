631a5ba7547f93b482c7cf4dfb0db8a04c13fe5a
root <root@DESKTOP-46789GH.localdomain>
2023-09-13 12:32:26 +0300

    Tasks from 0 to 4

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,3 +1,8 @@
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
 /**
  * op_add - This function used to return the sum of a and b
  *
