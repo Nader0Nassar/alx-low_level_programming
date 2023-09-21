8c8d169f41c86054e71461e2c95286bc03a0c20e
Nader0Nassar <nader.reda.ebrahim@gmail.com>
2023-09-22 01:18:58 +0300

    Task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * before_main - This function prints sentence before main
+ */
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
