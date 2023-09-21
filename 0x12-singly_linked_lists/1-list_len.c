a9b82b402fedfd820edf6d0eeb04e764eaf4bcc1
Nader0Nassar <nader.reda.ebrahim@gmail.com>
2023-09-22 00:21:04 +0300

    Task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * list_len - This function computes the number of elements 
+ * @h: head.
+ * Return: length.
+ */
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
