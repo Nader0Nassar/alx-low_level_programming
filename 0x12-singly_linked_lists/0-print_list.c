840d569057607481d727ceb60543944ec3c89ae4
Nader0Nassar <nader.reda.ebrahim@gmail.com>
2023-09-21 20:50:06 +0300

    Task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,12 +3,25 @@
 /**
  * print_list - This function prints all elements
  *              of the list.
- * @h: list of elements
+ * @h: head.
  * Return: Number of elements.
  */
 size_t print_list(const list_t *h)
 {
+    const list_t *ptr = malloc(sizeof(list_t));
+    int count = 0;
+
     if (h->str == NULL)
-        printf("[0] (nil)");
-    
+        printf("[0] (nil)\n");
+    else
+    {
+        ptr = h;
+        while (ptr->next != NULL)
+        {
+            count++;
+            printf("[%d] %s\n", ptr->len, ptr->str);
+            ptr = ptr->next;
+        }
+    }    
+    return (count);
 }
