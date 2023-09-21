7ef1091f0f894eb09af19b5b94e05fe7c66c683c
Nader0Nassar <nader.reda.ebrahim@gmail.com>
2023-09-21 19:10:23 +0300

    Task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * print_list - This function prints all elements
+ *              of the list.
+ * @h: list of elements
+ * Return: Number of elements.
+ */
+size_t print_list(const list_t *h)
+{
+    if (h->str == NULL)
+        printf("[0] (nil)");
+    
+}
