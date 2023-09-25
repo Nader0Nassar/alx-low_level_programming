db0b4be3695fd764f99618ec740bcb0ebbf89ef4
Nader0Nassar <nader.reda.ebrahim@gmail.com>
2023-09-25 21:15:28 +0300

    Task 14

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+/**
+ * find_listint_loop - This function searchs for loops.
+ * @head: head.
+ * Return: pointer to start.
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *ptr1, *ptr2;
+
+    ptr1 = ptr2 = head;
+	if (head == NULL)
+		return (NULL);
+	while (ptr1 && ptr2 && ptr2->next)
+	{
+		ptr2 = ptr2->next->next;
+		ptr1 = ptr1->next;
+		if (ptr2 == ptr1)
+		{
+			ptr1 = head;
+			while (ptr1 != ptr2)
+			{
+				ptr1 = ptr1->next;
+				ptr2 = ptr2->next;
+			}
+			return (ptr2);
+		}
+	}
+	return (NULL);
+}
