d5cf964fa2b6b4abc2323931fde71bd8f353e92d
Nader0Nassar <nader.reda.ebrahim@gmail.com>
2023-09-25 18:59:03 +0300

    Task 11

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,43 +10,31 @@ size_t listint_len(const listint_t *h);
 */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new_node, *temp;
-	unsigned int i = 0, length = listint_len(*head);
+	listint_t *new_node, *temp = *head;
+	unsigned int counter = 0;
 
-	if (idx > length)
-	{
-		return (NULL);
-	}
 	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL || head == NULL)
+	if (!new_node || !head)
 		return (NULL);
-	temp = *head;
-	while (i < idx - 1)
-	{
-		temp = temp->next;
-		i++;
-	}
 	new_node->n = n;
-	new_node->next = temp->next;
-	temp->next = new_node;
-	return (new_node);
-}
-/**
- * listint_len - This function compute the length.
- * @h: header of list.
- * Return: Number of nodes.
-*/
-size_t listint_len(const listint_t *h)
-{
-	size_t counter = 0;
-
-	if (h != NULL)
+	new_node->next = NULL;
+	if (idx == 0)
 	{
-		while (h != NULL)
-		{
-			counter++;
-			h = h->next;
-		}
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
 	}
-	return (counter);
+	while (temp && counter < idx)
+	{
+		if (counter == idx - 1)
+		{
+			new_node->next = temp->next;
+			temp->next = new_node;
+			return (new_node);
+		}
+		else
+			temp = temp->next;
+		counter++;
+	}
+	return (NULL);
 }
