0045979da7333bb405c07d835b119ae80bdd8212
Nader0Nassar <nader.reda.ebrahim@gmail.com>
2023-09-25 18:50:30 +0300

    Task 11

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,7 +18,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	}
 	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
+	if (new_node == NULL || head == NULL)
 		return (NULL);
 	temp = *head;
 	while (i < idx - 1)
