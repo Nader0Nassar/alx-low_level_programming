bee2e0ac39b063b0a8a82ed667ae88b631180bf5
Nader0Nassar <nader.reda.ebrahim@gmail.com>
2023-09-25 18:45:28 +0300

    Task 11

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,16 +8,16 @@
 */
 void free_listint2(listint_t **head)
 {
-	listint_t *temp;
+	listint_t *temp1, *temp2;
 
 	if (*head == NULL)
 		return;
-	temp = *head;
-	while (temp != NULL)
+	temp1 = *head;
+	while (temp1 != NULL)
 	{
-		temp = temp->next;
-		free(*head);
-		*head = temp;
+		temp2 = temp1;
+		temp1 = temp1->next;
+		free(temp2);
 	}
 	*head = NULL;
 }
